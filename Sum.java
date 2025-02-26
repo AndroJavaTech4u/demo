5d9895c665d9d4765819e90edc7080f952a7c9f4
AndroJavaTech4u <103114328+AndroJavaTech4u@users.noreply.github.com>
2025-02-26 22:40:26 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,25 @@
+class Sum
+{
+
+public static void main(String[] args)
+{ 
+
+java.util.Scanner s=new java.util.Scanner(System.in);
+System.out.println("Eneter first number");
+int num1=s.nextInt();
+
+System.out.println("Eneter Scond. number");
+int num2=s.nextInt();
+
+int add=num1+num2;
+
+System.out.println("Sum="+add);
+
+} 
+
+}
+
+ 
+
+
+
